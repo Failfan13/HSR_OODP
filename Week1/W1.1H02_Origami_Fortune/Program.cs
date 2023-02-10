3eac84af7a1752c72e38edfef806bb370a03fe1b
Erik M <failfan13gta5@gmail.com>
2023-02-10 12:28:15 +0100

    Changes / Half week home opdr

---
+++ Week1/W1.1H02_Origami_Fortune/Program.cs
@@ -0,0 +1,26 @@
+﻿string color = "";
+int number, lengthWord, remainder;
+number = lengthWord = remainder = 0;
+
+while (color == "") {
+    Console.WriteLine("Pick a color (red/blue/green/yellow):");
+    string? choiceColor = Console.ReadLine()?.ToLower();
+    if (new [] {"red", "blue", "green", "yellow"}.Contains(choiceColor)) {
+        color = choiceColor;
+        break;
+    }
+}
+while (number == 0) {
+    Console.WriteLine("Pick a number (1-8):");
+    int choiceNumber = Convert.ToInt32(Console.ReadLine()?.ToLower());
+    if (choiceNumber >= 1 && choiceNumber <= 8) {
+        number  = choiceNumber;
+        break;
+    }
+}
+
+lengthWord = color.Length;
+
+remainder = ((lengthWord + number) % 4);
+
+var fortuneNumber = ++remainder;
---
+++ Week1/W1.1H03_Fix_Syntax/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+int discount = 0.1;
+int price = 55;
+int discountPrice = price * discount;
+
+var message;
+message = $"The discount price is {discountPrice}";
+Console.WriteLine(discountPrice)
+*/
+
+
+double discount = 0.1;
+int price = 55;
+double discountPrice = price - price * discount;
+
+var message = "";
+message = $"The discount price is {discountPrice}";
+Console.WriteLine(message);
