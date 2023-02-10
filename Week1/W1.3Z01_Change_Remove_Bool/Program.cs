2b7fe2b8386560ad082edb1c27dfe0c7d8b2eec1
Erik M <failfan13gta5@gmail.com>
2023-02-10 12:52:58 +0100

    Add / Extra opdr

---
+++ Week1/W1.3Z01_Change_Remove_Bool/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program {
+    static void Main() {
+        bool pass = true, retakes = false;
+
+        Dictionary<string, List<double>> grades = new Dictionary<string, List<double>> ();
+        grades.Add("English", new List<double>{5});
+        grades.Add("Math", new List<double>{6.5});
+
+        foreach (KeyValuePair<string, List<double>> grade in grades) {
+            
+            while (grades[grade.Key][grades[grade.Key].Count-1] < 5.5) {
+                grades[grade.Key].Add(grades[grade.Key][grades[grade.Key].Count-1]+1);
+                retakes = true;
+                if (grades[grade.Key].Count >= 3) {
+                    break;
+                }
+            };
+
+            if (grades[grade.Key][(grades[grade.Key].Count-1)] < 5.5) {
+                pass = false;
+                Console.WriteLine($"Did you pass {grade.Key}? false");
+            } else {
+                Console.WriteLine($"Did you pass {grade.Key}? true");
+            }
+        };
+
+        Console.WriteLine($@"Did you need to do any retakes? {retakes.ToString().ToLower()}
+Did you pass this semester? {pass.ToString().ToLower()}");
+    }
+}
---
+++ Week1/W1.3Z02_Change_Remove_Double_Int/Program.cs
@@ -0,0 +1,9 @@
+﻿double input = Convert.ToDouble(Console.ReadLine());
+
+int Euro, Cents;
+
+Euro = (int)input;
+
+Cents = Convert.ToInt32(input.ToString().Replace($"{Euro}.", ""));
+
+Console.WriteLine($"Euros: {Euro}\nCents: {Cents}");
