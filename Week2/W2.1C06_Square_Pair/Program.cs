865ca3965e523650f0dae56903a1d4ad370ee92d
Erik M <failfan13gta5@gmail.com>
2023-02-10 21:54:48 +0100

    Add / opdr 1 - 9

---
+++ Week2/W2.1C06_Square_Pair/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program {
+    public static void Main() {
+        var square1 = new Square(5);
+        var square2 = new Square(10);
+
+        List<Square> squares = new List<Square> {square1, square2};
+        foreach (var square in squares) {
+            Console.WriteLine($"Side: {square.Side}");
+            Console.WriteLine($"Area: {square.Area()}");
+            Console.WriteLine($"Perimeter: {square.Perimeter()}");
+            // Console.WriteLine($"Side: {square.Side()}\nArea: {square.Area()}\nPerimeter: {square.Perimeter()}");
+        };
+    }
+}
