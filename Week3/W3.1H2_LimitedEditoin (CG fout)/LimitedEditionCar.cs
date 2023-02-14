a95a1e791c03ed063ba30f7120940547a386bb00
Erik M <failfan13gta5@gmail.com>
2023-02-14 14:04:47 +0100

    Change / huiswerk opdr 2

--- Week3/W3.1H2_LimitedEditoin (CG fout)/LimitedEditionCar.cs
+++ Week3/W3.1H2_LimitedEditoin (CG fout)/LimitedEditionCar.cs
@@ -1,9 +1,9 @@
 class LimitedEditionCar {
     public const string Model = "McLaren P1";
 
-    public static int Counter = 1;
+    public static int Counter;
 
-    public readonly string ChassisNumber = $"CHNO{Counter}";
+    public readonly string ChassisNumber;
 
-    public LimitedEditionCar() => ++Counter;
+    public LimitedEditionCar() => ChassisNumber = $"CHNO{++Counter}";
 }
