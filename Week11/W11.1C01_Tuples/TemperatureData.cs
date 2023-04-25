6379e864c8827a858a2d90d75c6341ad20129b18
Erik Maan <erikmaan02@gmail.com>
2023-04-25 11:01:52 +0200

    class opdr1

---
+++ Week11/W11.1C01_Tuples/TemperatureData.cs
@@ -0,0 +1,9 @@
+public class TemperatureData
+{
+    public (double, double) GetSalaryRange(double v1, double v2)
+    {
+        double minSalary = v1;
+        double maxSalary = v2;
+        return (minSalary, maxSalary);
+    }
+}
